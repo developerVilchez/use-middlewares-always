345b8ced841843e39887d022d565e4d173a4e21a
soporteclientesanonimos <developervilchez@gmail.com>
2020-10-28 18:27:53 -0500

    inicia proyecto sin middlewares

---
+++ app.js
@@ -0,0 +1,115 @@
+const express = require('express');
+const app = express();
+const jellybeanBag = require('./data')
+console.log(jellybeanBag)
+
+app.use(express.static('public'))
+
+//Rutas 
+/* Obtienes todos los osos*/
+app.get('/beans/', (req, res, next) => {
+  console.log('GET Request Received');
+  res.send(jellybeanBag);
+  console.log('Response Sent');
+})
+
+//Obtener oso por nombre
+app.get('/beans/:beanName', (req, res, next) => {
+  console.log('GET Request Received');
+  const beanName = req.params.beanName;
+  if (!jellybeanBag[beanName]) {
+    console.log('Response Sent');
+    return res.status(404).send('Bean with that name does not exist');
+  }
+  res.send(jellybeanBag[beanName]);
+  console.log('Response Sent');
+});
+
+//Crear un odo
+app.post('/beans/', (req, res, next) => {
+  console.log('POST Request Received');
+  
+  let queryData = '';
+
+  req.on('data', (data) => {
+    queryData += data;
+  });
+
+  req.on('end', () => {
+    const body = JSON.parse(queryData);
+    const beanName = body.name;
+    if (jellybeanBag[beanName] || jellybeanBag[beanName] === 0) {
+      return res.status(400).send('Bean with that name already exists!');
+    }
+    const numberOfBeans = Number(body.number) || 0;
+    jellybeanBag[beanName] = {
+      number: numberOfBeans
+    };
+    res.send(jellybeanBag[beanName]);
+    console.log('Response Sent');
+  });
+});
+
+//Aumentar el stock de osos
+app.post('/beans/:beanName/add', (req, res, next) => {
+  logRequest('POST');
+  const beanName = req.params.beanName;
+  if (!jellybeanBag[beanName]) {
+    return res.status(404).send('Bean with that name does not exist');
+  }
+  let queryData = '';
+  req.on('data', (data) => {
+    queryData += data;
+  });
+
+  req.on('end', () => {
+    const numberOfBeans = Number(JSON.parse(queryData).number) || 0;
+    jellybeanBag[beanName].number += numberOfBeans;
+    res.send(jellybeanBag[beanName]);
+    console.log('Response Sent');
+  });
+});
+
+
+//Eliminar una cantidad de osos, disminuir el stock de osos
+app.post('/beans/:beanName/remove', (req, res, next) => {
+  logRequest('POST');
+  const beanName = req.params.beanName;
+  if (!jellybeanBag[beanName]) {
+    return res.status(404).send('Bean with that name does not exist');
+  }
+  let queryData = '';
+  req.on('data', (data) => {
+    queryData += data;
+  });
+
+  req.on('end', () => {
+    const numberOfBeans = Number(JSON.parse(queryData).number) || 0;
+    if (jellybeanBag[beanName].number < numberOfBeans) {
+      return res.status(400).send('Not enough beans in the jar to remove!');
+    }
+    jellybeanBag[beanName].number -= numberOfBeans;
+    res.send(jellybeanBag[beanName]);
+    console.log('Response Sent');
+  });
+});
+
+//Borrar un oso de la base de datos
+app.delete('/beans/:beanName', (req, res, next) => {
+  logRequest('DELETE');
+  const beanName = req.params.beanName;
+  if (!jellybeanBag[beanName]) {
+    return res.status(404).send('Bean with that name does not exist');
+  }
+  jellybeanBag[beanName] = null;
+  res.status(204).send();
+  console.log('Response Sent');
+});
+
+
+const PORT = 4001 || process.env.PORT;
+
+
+app.listen(PORT, () => {
+  console.log(`Server listenning in port ${PORT}`)
+})
---
+++ data.js
@@ -0,0 +1,19 @@
+const jellybeanBag = {
+  mystery: {
+    number: 4
+  },
+  lemon: {
+    number: 5
+  },
+  rootBeer: {
+    number: 25
+  },
+  cherry: {
+    number: 3
+  },
+  licorice: {
+    number: 1
+  }
+};
+
+module.exports = jellybeanBag
